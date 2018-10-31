abcb318e2cd18d4602a625ebfa11ef39ad1bf945
Marc Poppy <poppy.marc@gmail.com>
2018-10-31 06:31:33 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -151,11 +151,11 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
-      expect(rows[0]).to.eq(" X | X | X ")
-      expect(rows[1]).to.eq("-----------")
-      expect(rows[2]).to.eq(" X | X | X ")
-      expect(rows[3]).to.eq("-----------")
-      expect(rows[4]).to.eq(" X | X | X ")
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
 
 
       # Each line that starts with expect represents a row in the ouput.
@@ -198,11 +198,11 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
-      expect(rows[0]).to.eq(" O | O | O ")
-      expect(rows[1]).to.eq("-----------")
-      expect(rows[2]).to.eq(" O | O | O ")
-      expect(rows[3]).to.eq("-----------")
-      expect(rows[4]).to.eq(" O | O | O ")
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | O | O ")
 
 
       #expect(true).to be(true)
